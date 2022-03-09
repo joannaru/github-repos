91cd0a94e7269c3f239bde5ace71c20b5fa0b84b
Joanna <79841668+joannaru@users.noreply.github.com>
2022-03-09 09:40:26 +0100

    delete unnecessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { DetailsModule } from './modules/details/details.module';
-import { ListModule } from './modules/list/list.module';
 
 @NgModule({
   declarations: [
